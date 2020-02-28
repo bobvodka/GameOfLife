0f94e5cdfb3707cbdccb061331779f6f89bbe523
Robert Jones <rob@phantom-web.co.uk>
2020-02-28 15:37:12 +0000

    Added comment about why the sync system exists as I once forgot.

--- GameOfLifeV2/Assets/Scripts/CellStateUpdateSystem.cs
+++ GameOfLifeV2/Assets/Scripts/CellStateUpdateSystem.cs
@@ -28,6 +28,15 @@ namespace LifeUpdateSystem
             renderSyncCmdBufferSystem = World.GetOrCreateSystem<CellRendererUpdateCommandBufferSystem>();
         }
 
+        // This system syncs the new renderables to the board entities which use them.
+        // This is needed because when we instanitate the entity via the command buffer while get
+        // an ID back that ID is only valid in that context as it is a negative number.
+        // When the command buffer is executed a new, real, Entity ID is created and the data patched
+        // to point at it, however if we have already stored the ID in the component attached to a parent
+        // we end up storing a bad ID and things will break.
+        // the correct IDs.
+        // This system executes after the new entities have been really spawned and lets us give things
+
         protected override void OnUpdate()
         {
 
